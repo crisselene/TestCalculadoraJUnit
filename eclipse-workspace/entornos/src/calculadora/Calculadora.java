6dd0cefe6c823fbf22b5c7725c047b65adea75c2
crisselene <criisalways@gmail.com>
2018-02-28 10:07:30 +0100

    "tests y test suit"

--- eclipse-workspace/entornos/src/calculadora/Calculadora.java
+++ eclipse-workspace/entornos/src/calculadora/Calculadora.java
@@ -24,4 +24,23 @@ public class Calculadora {
 	public int division() {
 		return num1/num2;
 	}
+	
+	public Integer division2() {
+		if(num2==0)return null;
+		else return num1/num2;
+	}
+	
+	public boolean num1EsMayor() {
+		if (num1>num2) return true;
+		else return false;
+			
+	}
+	
+	public int division3() {
+		if(num2==0)
+			throw new java.lang.ArithmeticException("División por 0");
+		else 
+			return num1/num2;
+	}
 }
+
